5159948a489297b8d26d323413be6ffd55b31fcf
James Bowden <jamesbowd@gmail.com>
2020-09-15 21:36:12 +0100

    created basic addition

--- app.js
+++ app.js
@@ -16,15 +16,54 @@ const divide = (int1, int2) => {
 
 const operate = (operator, int1, int2) => {
   if (operator === "add") {
-    add(int1, int2);
+    return add(int1, int2);
   }
   if (operator === "subtract") {
-    addsubtract(int1, int2);
+    return addsubtract(int1, int2);
   }
   if (operator === "multiply") {
-    multiply(int1, int2);
+    return multiply(int1, int2);
   }
   if (operator === "divide") {
-    divide(int1, int2);
+    return divide(int1, int2);
   }
 };
+
+const display = document.querySelector(".display");
+const keys = document.querySelectorAll(".btn");
+let operator = "";
+let int1 = 0;
+let int2 = 0;
+let lastAction;
+
+for (let key of keys) {
+  const action = key.dataset.action;
+  const keyContent = key.textContent;
+
+  key.addEventListener("click", () => {
+    const displayedNum = display.textContent;
+    if (!action) {
+      if (displayedNum === "0" || lastAction === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+      lastAction = "number";
+    }
+    if (action === "point") {
+      display.textContent = displayedNum + ".";
+      lastAction = "point";
+    }
+    if (action === "add") {
+      int1 = parseFloat(display.textContent);
+      operator = "add";
+      lastAction = "operator";
+    }
+    if (action === "equals") {
+      int2 = parseFloat(display.textContent);
+      answer = operate(operator, int1, int2);
+      display.textContent = answer;
+      lastAction = "equals";
+    }
+  });
+}
