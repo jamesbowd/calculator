e7d1246765402983c83b49a71b8d1d7d7579e97d
James Bowden <jamesbowd@gmail.com>
2020-09-23 21:29:58 +0100

    added more functiona;ity and styling

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const multiply = (int1, int2) => {
 };
 
 const divide = (int1, int2) => {
-  return int1 / int2;
+  if (int2 === 0) {
+    return "You cannot divide by zero";
+  } else {
+    return int1 / int2;
+  }
 };
 
 const operate = (operator, int1, int2) => {
@@ -19,7 +23,7 @@ const operate = (operator, int1, int2) => {
     return add(int1, int2);
   }
   if (operator === "subtract") {
-    return addsubtract(int1, int2);
+    return subtract(int1, int2);
   }
   if (operator === "multiply") {
     return multiply(int1, int2);
@@ -29,12 +33,33 @@ const operate = (operator, int1, int2) => {
   }
 };
 
+const equals = (newAction) => {
+  int2 = parseFloat(display.textContent);
+  answer = operate(operator, int1, int2);
+  display.textContent = answer;
+  lastAction = newAction;
+  int1 = parseFloat(display.textContent);
+};
+
+const actionOperator = (action) => {
+  if (int1 !== 0 && lastAction !== "equals") {
+    equals("operator");
+  } else {
+    int1 = parseFloat(display.textContent);
+    operator = action;
+    lastAction = "operator";
+  }
+  hasPoint = false;
+};
+
 const display = document.querySelector(".display");
 const keys = document.querySelectorAll(".btn");
+
 let operator = "";
 let int1 = 0;
 let int2 = 0;
 let lastAction;
+let hasPoint = false;
 
 for (let key of keys) {
   const action = key.dataset.action;
@@ -51,19 +76,32 @@ for (let key of keys) {
       lastAction = "number";
     }
     if (action === "point") {
-      display.textContent = displayedNum + ".";
-      lastAction = "point";
+      if (hasPoint === false) {
+        display.textContent = displayedNum + ".";
+        lastAction = "point";
+        hasPoint = true;
+      }
     }
     if (action === "add") {
-      int1 = parseFloat(display.textContent);
-      operator = "add";
-      lastAction = "operator";
+      actionOperator("add");
+    }
+    if (action === "subtract") {
+      actionOperator("subtract");
+    }
+    if (action === "multiply") {
+      actionOperator("multiply");
+    }
+    if (action === "divide") {
+      actionOperator("divide");
     }
     if (action === "equals") {
-      int2 = parseFloat(display.textContent);
-      answer = operate(operator, int1, int2);
-      display.textContent = answer;
-      lastAction = "equals";
+      equals("equals");
+    }
+    if (action === "clear") {
+      operator = "";
+      int1 = 0;
+      int2 = 0;
+      display.textContent = "0";
     }
   });
 }
